a87ea2df34141e23c5fadac77f0a6b70b837df7e
Antic Milovan <milovan.antic314@gmail.com>
2024-02-22 06:56:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 let buttonColours = ["red", "blue", "green", "yellow"];
-
 let gamePattern = [];
 let userClickedPattern = [];
 let started = false;
@@ -12,39 +11,27 @@ $(document).keypress(function() {
   }
 });
 $(".btn").click(function() {
-
   let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
   checkAnswer(userClickedPattern.length-1);
 });
 
 function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-      console.log("success");
-
       if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
           levelUP();
         }, 1000);
       }
-
     } else {
-
-      console.log("wrong answer!");
-
       playSound("wrong");
-
       $("body").addClass("game-over");
       setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
-
       $("#level-title").text("Game over. Press a button to start");
 
       //Rerun if the game is over
